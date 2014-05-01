5cfd44792cd04172bbca5be8b8e8aa1f56b4cc49
Shinmera <shinmera@tymoon.eu>
2014-05-01 18:27:22 +0200

    Temporary output fix

--- output.lisp
+++ output.lisp
@@ -12,12 +12,13 @@
 (defun save-configuration (path &key (format *output-format*) (object *config*))
   "Save the configuration OBJECT to PATH using the given FORMAT."
   (with-open-file (stream path :direction :output :if-does-not-exist :create :if-exists :supersede)
-    (%save format stream (serialize object))))
+    (%save format stream (serialize object)))
+  path)
 
-(defun load-configuaration (path &key (format *output-format*))
+(defun load-configuration (path &key (format *output-format*))
   "Load the configuration from PATH with the given FORMAT."
   (with-open-file (stream path :direction :input :if-does-not-exist :error)
-    (setf *config* (deserialize (%load format path)))))
+    (setf *config* (deserialize (%load format stream)))))
 
 (defgeneric %save (format path object))
 
@@ -33,8 +34,28 @@
   `(defmethod %load ((,(gensym "FORMAT") (eql ,(intern (string name) "KEYWORD"))) ,streamvar)
      ,@body))
 
+
+(defgeneric %print (object stream)
+  (:method ((table hash-table) stream)
+    (%print (loop with vector = (make-array (* 2 (hash-table-count table)))
+                  for i below (/ (array-dimension vector 0) 2)
+                  for k being the hash-keys of table
+                  for v being the hash-values of table
+                  do (setf (aref vector (* 2 i)) k
+                           (aref vector (1+ (* 2 i))) v)
+                  finally (return vector)) stream))
+  (:method ((string string) stream)
+    (print string stream))
+  (:method ((vector vector) stream)
+    (write-string "#(" stream)
+    (loop for item across vector
+          do (%print item stream))
+    (write-string ")" stream))
+  (:method (object stream)
+    (print object stream)))
+
 (define-save-format lisp (stream object)
-  (print object stream))
+  (%print object stream))
 
 (define-load-format lisp (stream)
   (read stream))
