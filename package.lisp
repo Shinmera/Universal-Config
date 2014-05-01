8478d6ba0e0622c7ee5a6e693ce80d7509cce518
Shinmera <shinmera@tymoon.eu>
2014-05-01 17:50:21 +0200

    Updated exports

--- package.lisp
+++ package.lisp
@@ -7,5 +7,42 @@
 (defpackage universal-config
   (:nicknames #:org.tymoonnext.universal-config #:uc)
   (:use #:cl)
+  ;; access.lisp
   (:export
-   ))
+   #:*config*
+   #:*augment-missing-places*
+   
+   #:inexistent-place
+   #:accessor
+   #:object
+   
+   #:augmenting-place
+   #:accessor
+   #:object
+   
+   #:access
+   #:make-container
+   #:config-tree)
+  ;; output.lisp
+  (:export
+   #:*output-format*
+   
+   #:save-configuration
+   #:load-configuration
+   
+   #:define-save-format
+   #:define-load-format)
+  ;; transform.lisp
+  (:export
+   #:escape
+   #:unescape
+   #:split-escaped
+   
+   #:serialize
+   #:define-string-serializer
+   #:define-serializer
+   
+   #:deserialize
+   #:define-string-deserialize
+   #:define-deserializer)
+  )
